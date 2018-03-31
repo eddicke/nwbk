2c0768c90248f87dcc193371e62a064d6756e02d
eddicke <adenekanademola931@gmail.com>
2018-03-31 18:07:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ io.on('connection', function(socket) {
   io.emit("oldMsg", oldMsg);
   socket.on('chat', function(username, message) {
     console.log('message received, sent by: ' + username + ', content: ' + message);
-    oldMsg.push('message received, sent by: ' + username + ', content: ' + message);
+   // oldMsg.push('message received, sent by: ' + username + ', content: ' + message);
     io.emit('chat', username, message);
   });
  socket.on('ademola', function(gme){
